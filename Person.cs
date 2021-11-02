9cbaffb9443e20f7443bf8774445df2aa6b47390
Ysh1da <92174112+Ysh1da@users.noreply.github.com>
2021-11-02 20:07:08 +0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Persona
+{
+    public class Person
+    {
+
+        private string surname;
+        private string name;
+        private string patronymic;
+        private int age;
+        private double weight;
+        private string colorEyes;
+        public Person(string surname, string name, string patronymic, int age, double weight, string colorEyes)
+        {
+            this.surname = surname;
+            this.name = name;
+            this.patronymic = patronymic;
+            this.colorEyes = colorEyes;
+            this.age = age;
+            this.weight = weight;
+
+        }
+
+        public void runInWork()
+        {
+            Console.WriteLine("*Сломя голову несусь на работу*");
+        }
+        public void eatDonut()
+        {
+            Console.WriteLine("*С трудом идя на работу, кушаю пончик*");
+        }
+        public void sleeping()
+        {
+            Console.WriteLine("*Сплю...Zzzzz...*");
+        }
+        public void eatSoup()
+        {
+            Console.WriteLine("*Жадно ем на обеде суп!!*");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Моя фамилия - " + surname + ", моё имя - " + name + ", моё отчество - " + patronymic + ",\nи мне всего лишь " + age + " лет , а вешу я всего ничего, " + weight
+                              + "киллограм ,\nглаза у меня красивые, " + colorEyes + " цвета.");
+
+        }
+    }
+}
+
